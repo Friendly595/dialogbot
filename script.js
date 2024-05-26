74fec7e56916b55c6c8675d294357c1e53653798
Friendly595 <87995042+Friendly595@users.noreply.github.com>
2024-05-26 12:43:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+cef.on('updateBotDialog', (botName, botReplica, buttonTexts) => {
+    updateBotDialog(botName, botReplica, buttonTexts.split('|'));
+});
+
+function updateBotDialog(botName, botReplica, buttonTexts) {
+    // Обновляем имя бота
+    document.getElementById('bot-name').innerText = botName;
+    // Обновляем реплику бота
+    document.getElementById('bot-replica').innerText = botReplica;
+    // Очищаем текущие кнопки
+    const buttonsContainer = document.getElementById('buttons');
+    buttonsContainer.innerHTML = '';
+
+    // Создаем новые кнопки на основе переданного массива buttonTexts
+    buttonTexts.forEach((text, index) => {
+        if (text.trim() !== "") {
+            const buttonDiv = document.createElement('div');
+            buttonDiv.className = `button${index + 1}`;
+            const buttonTextDiv = document.createElement('div');
+            buttonTextDiv.className = `button${index + 1}-text`;
+            buttonTextDiv.innerText = text;
+
+            // Добавляем обработчик события клика
+            buttonDiv.addEventListener('click', () => {
+                sendButtonIDToPawn(index + 1); // Отправляем ID кнопки
+            });
+
+            buttonDiv.appendChild(buttonTextDiv);
+            buttonsContainer.appendChild(buttonDiv);
+			document.querySelector('.talk-bot').classList.remove('hidden');
+        }
+    });
+}
+
+// Функция для отправки ID кнопки в Pawn
+function sendButtonIDToPawn(buttonID) {
+    cef.emit('buttonClicked', buttonID);
+}
